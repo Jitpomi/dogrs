2a341f139368c8d66cca3f19feefaf04fd5d35df
jermsam <ssali@jitpomi.com>
2025-12-16 14:15:17 -0700

    blog-axum: simplify BlogParams (type alias)

--- dog-examples/blog-axum/src/services/types.rs
+++ dog-examples/blog-axum/src/services/types.rs
@@ -1,31 +1,7 @@
 use std::collections::HashMap;
-use std::ops::{Deref, DerefMut};
 
-use dog_axum::params::{FromRestParams, RestParams};
+pub type BlogParams = dog_axum::params::RestParams;
 use tokio::sync::RwLock;
-
-#[derive(Debug, Clone, Default)]
-pub struct BlogParams(pub RestParams);
-
-impl Deref for BlogParams {
-    type Target = RestParams;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl DerefMut for BlogParams {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
-
-impl FromRestParams for BlogParams {
-    fn from_rest_params(params: RestParams) -> Self {
-        Self(params)
-    }
-}
 
 #[derive(Default)]
 pub struct BlogState {
