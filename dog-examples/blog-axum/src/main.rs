8c34b0622d8850bcbc2675befc2f6306e80e2a77
samson.ssali <samson.ssali@sparkz.systemz>
2026-06-08 11:17:58 -0600

    chore: add dotenvy support across all example apps

--- dog-examples/blog-axum/src/main.rs
+++ dog-examples/blog-axum/src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
